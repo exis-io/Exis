bb8d81382749f46a5f49e507fc889c2fee1ec61b
Mickey Barboi <damouse007@gmail.com>
2015-09-27 22:26:30 -0500

    pushing to test update

--- Pod/Classes/Riffle.swift
+++ Pod/Classes/Riffle.swift
@@ -8,25 +8,141 @@
 
 import Foundation
 
-public class BlinkingLabel : UILabel {
-    public func startBlinking() {
-        let options : UIViewAnimationOptions = UIViewAnimationOptions.Repeat
-        UIView.animateWithDuration(0.25, delay:0.0, options:options, animations: {
-            self.alpha = 0
-        }, completion: nil)
+let NODE = "ws://ubuntu@ec2-52-26-83-61.us-west-2.compute.amazonaws.com:8000/ws"
+//let NODE = "ws://localhost:8000/ws"
 
+
+// Sets itself as the delegate if none provided
+@objc protocol RiffleDelegate {
+    func onJoin()
+    func onLeave()
+}
+
+
+class RiffleSession: NSObject, MDWampClientDelegate, RiffleDelegate {
+    var socket: MDWampTransportWebSocket
+    var session: MDWamp
+    
+    var delegate: RiffleDelegate?
+    
+    
+    init(pdid: String) {
+        socket = MDWampTransportWebSocket(server:NSURL(string: NODE), protocolVersions:[kMDWampProtocolWamp2msgpack, kMDWampProtocolWamp2json])
+        
+        // Oh, the hacks you'll see
+        session = MDWamp()
+        super.init()
+        
+        session = MDWamp(transport: socket, realm: pdid, delegate: self)
     }
     
-    public func stopBlinking() {
-        alpha = 1
-        layer.removeAllAnimations()
+    func connect() {
+        if delegate == nil {
+            delegate = self
+        }
+        
+        session.connect()
     }
     
-    public func doSomething() -> String {
-        let a = MDWamp()
-        return ""
+    func handle(args: AnyObject...) {
+        
+    }
+    
+    //MARK: Delegates
+    func mdwamp(wamp: MDWamp!, sessionEstablished info: [NSObject : AnyObject]!) {
+        print("Session Established!")
+        delegate!.onJoin()
+    }
+    
+    func mdwamp(wamp: MDWamp!, closedSession code: Int, reason: String!, details: [NSObject : AnyObject]!) {
+        print("Session Closed!")
+        delegate!.onLeave()
+    }
+    
+    func onJoin() {
+        // Called when a session closes. Setup here.
+    }
+    
+    func onLeave() {
+        // called when a session closes. Do any cleanup here
+    }
+    
+    
+    //MARK: Messaging Patterns
+    func register(endpoint: String, callback: (AnyObject... ) -> ()) {
+        session.registerRPC(endpoint, procedure: { (wamp: MDWamp!, invocation: MDWampInvocation!) -> Void in
+            print("Someone called hello: ", invocation)
+            
+            //print("", invocation.request)
+            //print("", invocation.registration)
+            //print("", invocation.options)
+            //print("", invocation.arguments)
+            //print("", invocation.argumentsKw)
+            
+            callback(invocation.arguments)
+            
+            }, cancelHandler: { () -> Void in
+                print("Register Cancelled!")
+            }) { (err: NSError!) -> Void in
+                print("Registration completed.")
+        }
+    }
+    
+    func call(endpoint: String, args: AnyObject...) {
+        session.call(endpoint, payload: args) { (result: MDWampResult!, err: NSError!) -> Void in
+            if err != nil {
+                print("ERR: ", err)
+            }
+            else {
+                print("Call completed")
+            }
+        }
+    }
+    
+    func publish(endpoint: String, args: AnyObject...) {
+        session.publishTo(endpoint, payload: args, result: { (err: NSError!) -> Void in
+            if let e = err {
+                print("Error: ", e)
+            }
+        })
+    }
+    
+    func subscribe(endpoint: String, callback: (AnyObject...) -> ()) {
+        session.subscribe(endpoint, onEvent: { (event: MDWampEvent!) -> Void in
+            print("Sub came in: ", event)
+            
+            //print("", event.subscription)
+            //print("", event.publication)
+            //print("", event.topic)
+            print("", event.details)
+            print("", event.arguments)
+            //print("", event.argumentsKw)
+            //print("", event.event)
+            
+            callback(event.arguments)
+            
+            }) { (err: NSError!) -> Void in
+                if let e = err {
+                    print("An error occured: ", e)
+                }
+                else {
+                    print("Sub completed")
+                }
+        }
     }
 }
 
-//class RiffleSession: NSObject, MDWampClientDelegate {
-//}
+/*
+Getting the signature from provided handler:
+
+func f(a: Int, b: Int) {
+}
+
+let y = Mirror(reflecting: f)
+
+let types = y.subjectType
+print(types)
+
+
+*/
+
--- Riffle.podspec
+++ Riffle.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = "Riffle"
-  s.version          = "0.1.0"
+  s.version          = "0.1.1"
   s.summary          = "A short description of Riffle."
 
 # This description is used to generate tags and improve search results.
@@ -38,7 +38,7 @@ Pod::Spec.new do |s|
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
 
-s.dependency 'SocketRocket'
-s.dependency 'MPMessagePack'
-s.dependency 'CocoaAsyncSocket'
+    s.dependency 'SocketRocket'
+    s.dependency 'MPMessagePack'
+    s.dependency 'CocoaAsyncSocket'
 end
