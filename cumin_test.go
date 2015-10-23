e51f011be9ed340d0ec9b4dc35f3cbadd7b51011
damouse <mickey.barboi@gmail.com>
2015-10-23 18:33:05 -0500

    all green

---
+++ cumin_test.go
@@ -0,0 +1,19 @@
+package riffle
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestCuminConvert(t *testing.T) {
+	Convey("No args functions should be cumincable", t, func() {
+		// r, e = Cumin(noneNone, []interface{}())
+		// So(e, ShouldBeNil)
+	})
+}
+
+// Functions for cuminication
+func noneNone() {
+
+}
