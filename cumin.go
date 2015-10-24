6f2a81177d6a816f2f51d533abd0cb7736765248
Damouse <mickey.barboi@gmail.com>
2015-10-24 14:34:12 -0500

    number of args checking

--- cumin.go
+++ cumin.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 )
 
+// Convert and apply args to arbitrary function fn
 func Cumin(fn interface{}, args []interface{}) ([]interface{}, error) {
 	reciever := reflect.TypeOf(fn)
 	var ret []interface{}
@@ -14,23 +15,22 @@ func Cumin(fn interface{}, args []interface{}) ([]interface{}, error) {
 		return ret, fmt.Errorf("Handler is not a function!")
 	}
 
+	if reciever.NumIn() != len(args) {
+		return ret, fmt.Errorf("Cumin ERR: expected %s args for function %s, got %s", reciever.NumIn(), reciever, len(args))
+	}
+
 	// Iterate over the params listed in the method and try their casts
 	values := make([]reflect.Value, len(args))
 	for i := 0; i < reciever.NumIn(); i++ {
 		param := reciever.In(i)
 		arg := reflect.ValueOf(args[i])
 
-		// If the types don't match exactly attempt a construction
-		if param != arg.Type() {
-			// Try a conversion
-			if arg.Type().ConvertibleTo(param) {
-				values[i] = arg.Convert(param)
-				continue
-			}
-
-			return ret, fmt.Errorf("Cuminication failed. Expected %s for arg[%d] in  (%s), got %s.", param, i, reciever, arg.Type())
-		} else {
+		if param == arg.Type() {
 			values[i] = arg
+		} else if arg.Type().ConvertibleTo(param) {
+			values[i] = arg.Convert(param)
+		} else {
+			return ret, fmt.Errorf("Cumin ERR: expected %s for arg[%d] in (%s), got %s.", param, i, reciever, arg.Type())
 		}
 	}
 
@@ -41,7 +41,7 @@ func Cumin(fn interface{}, args []interface{}) ([]interface{}, error) {
 		ret = append(ret, x.Interface())
 	}
 
-	// Catch any exceptions this produces and pass them to the function that sent them... or a handler block?
+	// Catch any exceptions this produces and pass them to the function that sent them, or some kind of handler
 
 	return ret, nil
 }
