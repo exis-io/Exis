b0a3110784756289354267162f4f119948acd0d6
damouse <damouse007@gmail.com>
2015-12-01 22:22:37 -0800

    last touches

--- sandbox/server.js
+++ sandbox/server.js
@@ -2,49 +2,37 @@ var riffle = require('jsriffle');
 riffle.setDevFabric();
 console.log("Starting server")
 
-var connection = new riffle.Connection('xs.damouse.backend');
-
-function onevent(args) {
-  console.log("Event:", args[0]);
+function subscriptionHandler(args) {
+    console.log("Publish received:", args[0]);
 };
 
-connection.onJoin = function (session) {
-    console.log("Connection opened");
-    
-    session.subscribe('xs.damouse.backend/sub', onevent).then(
-      function (registration) {
-         console.log("Subscription registered:", registration.id);
-      },
-      function (error) {
-         console.log("Registration failed:", error);
-      }
-   );
-
-   function utcnow() {
-      console.log("Call receive");
-      now = new Date();
-      return now.toISOString();
-   };
-
-   session.register('xs.damouse.backend/register', utcnow).then(
-      function (registration) {
-         console.log("Procedure registered:", registration.id);
-      },
-      function (error) {
-         console.log("Registration failed:", error);
-      }
-   );
+function registerHandler(args) {
+    console.log("Call received: ",  args[0]);
+    return "Pong"
 };
 
 var domain = new riffle.Domain("xs.demo");
 
-// var s = d.subdomain("bob");
-
 domain.onJoin = function() {
-    console.log("Domain override join");
-    this.subscribe();
+    console.log("Domain " + this.domain + " joined with name " + this.name);
+
+    this.subscribe('sub', subscriptionHandler).then(
+        function (registration) {
+            console.log("Subscription registered:", registration.id);
+        },
+        function (error) {
+            console.log("Subscription failed:", error);
+        }
+    );
+
+    this.register('register', registerHandler).then(
+        function (registration) {
+            console.log("Procedure registered:", registration.id);
+        },
+        function (error) {
+            console.log("Registration failed:", error);
+        }
+    );
 };
 
 domain.join();
-
-// connection.join();
