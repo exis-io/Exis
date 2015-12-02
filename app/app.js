2722604de6a25b6812b54945a793a53110394b99
damouse <mickey.barboi@gmail.com>
2015-12-02 17:35:39 -0600

    testing riffle setup

--- app/app.js
+++ app/app.js
@@ -1,12 +1,25 @@
 'use strict';
 
+
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  "vxWamp",
 ]).
+
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}])
+
+.config(function($wampProvider) {
+    $wampProvider.init({
+        url: 'ws://ubuntu@ec2-52-26-83-61.us-west-2.compute.amazonaws.com:8000/ws',
+        realm: 'xs.demo.angular'
+    });
+})
+.run(function($wamp){
+    $wamp.open();
+});
