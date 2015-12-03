5b824209c8573d6efc2ad70cefefb22b0f2057db
damouse <damouse007@gmail.com>
2015-12-03 08:15:41 -0600

    last touches on basic stability

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,31 +11,25 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', function($scope, $wamp) {
 
-   // 1) subscribe to a topic
-   function onevent(args) {
-      $scope.hello = args[0];
-   }
+    $wamp.register('xs.demo.angular/register', function(args) {
+        console.log("Ng received call: " + args[0] + args[1])
+    });
 
-   // $wamp.register('xs.demo.angular/register', function(args) {
-   //      console.log("Ng received call: " + args[0] + args[1])
-   // });
+    $wamp.subscribe('xs.demo.angular/sub', function(args) {
+        console.log("Ng received publish: " + args[0] + args[1])
+    });
 
-   // $wamp.subscribe('xs.demo.angular/sub', function(args) {
-   //      console.log("Ng received publish: " + args[0] + args[1])
-   // });
-
-    console.log("Attempting to publish")
     $wamp.publish('xs.demo.server/sub', 'Hello', 'world!');
 
-   $wamp.call('xs.demo.server/register', 'Ping, ', 'you dog');
+    $wamp.call('xs.demo.server/register', 'Ping, ', 'you dog');
       
 
    $scope.$on("$wamp.open", function (event, session) {
         console.log('We are connected to the WAMP Router!'); 
     });
 
-   //  $scope.$on("$wamp.close", function (event, data) {
-   //      $scope.reason = data.reason;
-   //      $scope.details = data.details;
-   //  });
+    $scope.$on("$wamp.close", function (event, data) {
+        $scope.reason = data.reason;
+        $scope.details = data.details;
+    });
 });
