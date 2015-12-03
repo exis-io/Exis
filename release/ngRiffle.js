5ca8d2bc74adf2ea8a380976c0b30e8fdad77b62
damouse <damouse007@gmail.com>
2015-12-03 06:40:48 -0600

    that took a while

--- release/ngRiffle.js
+++ release/ngRiffle.js
@@ -1,181 +1,28 @@
 /* commonjs package manager support */
 if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports){
     var autobahn = require('autobahn');
+    var jsriffle = require('jsriffle');
     module.exports = 'vxWamp';
 }
 
 (function () {
     'use strict';
 
-    /**
-     * @ngdoc module
-     * @name vxWamp
-     * @description
-     *
-     * # vxWamp
-     *
-     * The `vxWamp` module provides seamless WAMPv2 integration, by wrapping AutobahnJS, for angular apps.
-     *
-     * ## Example
-     * See {@link $wamp#example $wamp} for an example of configuring and using `vxWamp`.
-     *
-     *
-     * <div doc-module-components="vxWamp"></div>
-     */
-    /* global -vxWampModule */
     var vxWampModule = angular.module('vxWamp', []).provider('$wamp', $WampProvider);
 
-
-    /**
-     * @ngdoc provider
-     * @name $wampProvider
-     *
-     * @description
-     *
-     * Used for accessing a WAMP resource.
-     *
-     * ## Example
-     * See {@link $wamp#example $wamp} for an example of configuring and using `vxWamp`.
-     *
-     * ## Dependencies
-     * Requires the {@link vxWamp `vxWamp`} module to be installed.
-     */
     function $WampProvider() {
         var options;
 
-        /**
-         * @ngdoc method
-         * @name $wampProvider#init
-         *
-         * @param {Object} initOptions autobahnJS connection options
-         *
-         *    Object properties:
-         *
-         *      Required options:
-         *
-         *    - `url`: `{string=}` - the WebSocket URL of the WAMP router to connect to, e.g. ws://myserver.com:8080/ws
-         *    - `realm`: `{string=}` - The WAMP realm to join, e.g. realm1
-         *
-         *    Optional options:
-         *
-         *      Options that control what kind of Deferreds to use:
-         *
-         *      - `use_es6_promises`: `{boolean=}` - use deferreds based on ES6 promises
-         *      - `use_deferred`: `{callable=}` - if provided, use this deferred constructor, e.g. jQuery.Deferred or Q.defer (default: $q.defer)
-         *
-         *      Options that control automatic reconnection:
-         *
-         *      - `max_retries`: `{integer=}` - Maximum number of reconnection attempts (default: 15)
-         *      - `initial_retry_delay`: `{float=}` - Initial delay for reconnection attempt in seconds (default: 1.5).
-         *      - `max_retry_delay`: `{float=}` - Maximum delay for reconnection attempts in seconds (default: 300).
-         *      - `retry_delay_growth`: `{float=}` - The growth factor applied to the retry delay between reconnection attempts (default: 1.5).
-         *      - `retry_delay_jitter`: `{float=}` - The standard deviation of a Gaussian to jitter the delay on each retry cycle as a fraction of the mean (default: 0.1).
-         *      - `disable_digest`: `{boolean=}` - Disables wrapping all promises and callbacks with scope.$apply (default: false).
-         *
-         * @description
-         * Configures the AutobhanJS Service
-         */
         this.init = function (initOptions) {
             console.log("Access");
             options = initOptions || {};
         };
 
-        /**
-         * @ngdoc property
-         * @name $wampProvider#interceptors
-         * @description
-         *
-         * Array containing service factories for all synchronous or asynchronous
-         * postprocessing of responses.
-         *
-         * {@link $wamp#interceptors Interceptors detailed info}
-         **/
+
         var interceptors = this.interceptors = [];
 
         this.$get = ["$rootScope", "$q", "$log", "$injector", function ($rootScope, $q, $log, $injector) {
 
-            /**
-             * @ngdoc service
-             * @name $wamp
-             *
-             * @description
-             * `$wamp` give you access to the autobahnJS methods, call, register, subscribe, unsubscribe and publish.
-             *
-             * Requires the {@link vxWamp `vxWamp`} module to be installed.
-             *
-             * You can configure the WAMP connection through {@link $wampProvider}'s API.
-             *
-             * @example
-             *
-             * app.config(function ($wampProvider) {
-             *      $wampProvider.init({
-             *          url: 'ws://127.0.0.1:9000/',
-             *          realm: 'realm1'
-             *      });
-             * })
-             *
-             *  app.controller("MyCtrl", function($scope, $wamp) {
-             *
-             *      // 1) subscribe to a topic
-             *      function onevent(args) {
-             *          $scope.hello = args[0];
-             *      }
-             *      $wamp.subscribe('com.myapp.hello', onevent);
-             *
-             *      // 2) publish an event
-             *      $wamp.publish('com.myapp.hello', ['Hello, world!']);
-             *
-             *      // 3) register a procedure for remoting
-             *      function add2(args) {
-             *          return args[0] + args[1];
-             *      }
-             *      $wamp.register('com.myapp.add2', add2);
-             *
-             *      // 4) call a remote procedure
-             *      $wamp.call('com.myapp.add2', [2, 3]).then(
-             *          function (res) {
-             *          $scope.add2 = res;
-             *      });
-             * });
-             *
-             *  Events
-             *
-             *  There are four events that $wamp can broadcast:
-             *      1)  $wamp.open - is sent when the WAMP connection opens.
-             *
-             *              $scope.$on("$wamp.open", function (event, info) {
-             *                  // info.session
-             *                  // info.details
-             *                  // Do something
-             *              });
-             *
-             *      2)  $wamp.close - is sent when the WAMP connection closes.
-             *
-             *              $scope.$on("$wamp.close", function (event, info) {
-             *                  // info.reason: wamp close reason
-             *                  // info.details: wamp close details
-             *              });
-             *
-             *      3)  $wamp.error - is sent when an error occurs while trying to make a call, publish or register
-             *
-             *              $scope.$on("$wamp.error", function (event, error) {
-             *                  // error: Autobahn.Error object
-             *              });
-             *
-             *      4)  $wamp.onchallenge
-             *
-             *              $scope.$on("$wamp.onchallenge", function (event, info) {
-             *                  // info.promise: promise to return to wamp,
-             *                  // info.session: wamp session,
-             *                  // info.method: auth method,
-             *                  // info.extra: extra
-             *
-             *                  //ie. wamp-cra
-             *                  var key =  autobahn.auth_cra.derive_key("da_password", info.extra.salt);
-             *                  return info.promise.resolve(autobahn.auth_cra.sign(key, info.extra.challenge));
-             *              });
-             *
-             */
 
             var connection;
             var sessionDeferred = $q.defer();
@@ -422,7 +269,7 @@ if (typeof module !== "undefined" && typeof exports !== "undefined" && module.ex
                     });
                 },
                 hello: function () {
-                    console.log("Hello!");
+                    console.log(jsriffle.biddle);
                     return interceptorWrapper('call', arguments, function () {
                         return connection.session.call(procedure, args, kwargs, options);
                     });
