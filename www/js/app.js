a162eba834516fe03f08991a9da68f0981d3df08
Nicholas Hyatt <nhyatt@wisc.edu>
2015-12-03 15:48:59 -0600

    should be functioning ionic cards against app

--- www/js/app.js
+++ www/js/app.js
@@ -3,8 +3,34 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('cardsAgainst', ['ionic', 'vxWamp'])
 
+app.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider 
+  .state('home', {
+    url: '/',
+    templateUrl: 'templates/splash.html'
+  })
+  .state('room', {
+    url: '/room',
+    templateUrl: 'templates/room.html'
+  })
+})
+.config(['$wampProvider', function($wampProvider){
+  $wampProvider.init({
+    url: 'wss://node.exis.io:8000/ws',
+  }); 
+}])
+.config(function ($httpProvider) {
+  //disregard browser pre-flight checks
+  var contentType = { 'Content-Type' : 'application/x-www-form-urlencoded' };
+  for (var verb in $httpProvider.defaults.headers)
+  {
+    $httpProvider.defaults.headers[verb] = contentType;
+  }
+})
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
