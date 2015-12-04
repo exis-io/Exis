8d4424cbeca79bb58c85ddb1c885e0819fc2c9fc
damouse <damouse007@gmail.com>
2015-12-04 02:48:45 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# pyRiffle
+
+Riffle functionality for python. 
+
+As of this writing this repository is in an experimental state and is not ready for consumption.
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, Extension
+
+setup(
+    name="pyRiffle",
+    version="0.1.1",
+    author="Damouse",
+    description="Riffle client libraries for interacting over a fabric",
+    packages=['riffle'],
+    package_data={'riffle': ['libriff.so']}, 
+    runtime_library_dirs=['riffle'],
+    library_dirs=['riffle'],
+    include_package_data=True,
+    install_requires=[
+        'greenlet'
+    ],
+    entry_points={
+        'console_scripts': [
+            'reef=riffle.river:main',
+        ],
+    }
+)
