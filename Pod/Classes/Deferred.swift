0bba15f4bead7e7db40b4e7ca527045c2cbf8091
damouse <damouse@damouses-Mac.local>
2015-12-04 19:28:45 -0600

    integrated deferreds

--- Pod/Classes/Deferred.swift
+++ Pod/Classes/Deferred.swift
@@ -6,36 +6,59 @@
 //
 //  Homebaked, python Twisted inspired deferreds.
 //
-
+//  True twisted style chain hopping has not been implemented
 
 import Foundation
 
 
 public class Deferred {
-    var _callback: (() -> ())?
-    var _errback: (() -> ())?
+    var _callback: [((AnyObject?) -> AnyObject?)] = []
+    var _errback: [((AnyObject?) -> AnyObject?)] = []
     
     var fired = false
     
-    func succeed() {
-        if let cb = _callback {
-            fired = true
-            cb()
+    func callback() {
+        if fired {
+            print("Defereds can only fire once!")
+            return
         }
-    }
-    
-    func fail() {
-        if let cb = _errback {
-            fired = true
-            cb()
+        
+        fired = true
+        var result: AnyObject?
+        
+        while _callback.count > 0 {
+            let cb = _callback.removeFirst()
+            result = cb(result)
         }
+        
+        _errback = []
     }
     
-    func callback(cb: () -> ()) {
-        _callback = cb
+    func errback() {
+        if fired {
+            print("Defereds can only fire once!")
+            return
+        }
+        
+        fired = true
+        var result: AnyObject?
+        
+        while _errback.count > 0 {
+            let cb = _errback.removeFirst()
+            result = cb(result)
+        }
+        
+        _callback = []
     }
     
-    func errback(cb: () -> ()) {
-        _errback = cb
+    
+    func addCallback(cb: (AnyObject?) -> AnyObject?) -> Deferred {
+        _callback.append(cb)
+        return self
+    }
+    
+    func addErrback(cb: (AnyObject?) -> AnyObject?) -> Deferred {
+        _errback.append(cb)
+        return self
     }
 }
