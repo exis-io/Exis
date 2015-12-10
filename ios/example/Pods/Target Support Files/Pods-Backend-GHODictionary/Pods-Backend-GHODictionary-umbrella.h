57b7eaf458ce625d797f91e08caa6249d40d7973
Mickey Barboi <damouse007@gmail.com>
2015-12-10 16:47:38 -0600

    removed example app for subtree addition

--- ios/example/Pods/Target Support Files/Pods-Backend-GHODictionary/Pods-Backend-GHODictionary-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-#import "GHODict.h"
-#import "GHODictionary.h"
-
-FOUNDATION_EXPORT double GHODictionaryVersionNumber;
-FOUNDATION_EXPORT const unsigned char GHODictionaryVersionString[];
-
