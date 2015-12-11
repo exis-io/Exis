f4f3aea4062b0bfa8369aed51442799b7ef878d3
damouse <mickey.barboi@gmail.com>
2015-12-11 17:13:43 -0600

    updated cMantle to bring it in line with python mantle

--- core/cMantle/main.go
+++ core/cMantle/main.go
@@ -1,46 +1,118 @@
 // package name: riffle
-package main
+package riffle
 
 import (
 	"C"
-	"encoding/json"
-	"fmt"
 	"unsafe"
 
 	"github.com/exis-io/core"
 	"github.com/exis-io/core/goRiffle"
 )
 
-/*
-This is the lowest level core, just exposes the C API. Used for python, swift-linux, and osx.
-
-You are responsible for cleaning up C references!
-
-
-Every function here is reactive: it returns two indicies to callbacks to be triggered later.
-
-Reg, Sub, Pub, Call all return indicies to callbacks they will later call.
-*/
-
-// Required main method
-func main() {}
+//export main
+// func main() {}
 
 // By default always connect to the production fabric at node.exis.io
 var fabric string = core.FabricProduction
 
 //export NewDomain
-func NewDomain(name *C.char) unsafe.Pointer  {
-    d := core.NewDomain(C.GoString(name), nil)
-    return unsafe.Pointer(&d)
+func NewDomain(name *C.char) unsafe.Pointer {
+	d := core.NewDomain(C.GoString(name), nil)
+	return unsafe.Pointer(&d)
 }
 
 //export Subdomain
-func Subdomain(pdomain unsafe.Pointer, name *C.char) unsafe.Pointer  {
-    d := *(*core.Domain)(pdomain)
-    n := d.Subdomain(C.GoString(name))
-    return unsafe.Pointer(&n)
+func Subdomain(pdomain unsafe.Pointer, name *C.char) unsafe.Pointer {
+	d := *(*core.Domain)(pdomain)
+	n := d.Subdomain(C.GoString(name))
+	return unsafe.Pointer(&n)
 }
 
+//export Receive
+func Receive(pdomain unsafe.Pointer) string {
+	d := *(*core.Domain)(pdomain)
+	return core.MantleMarshall(d.GetApp().CallbackListen())
+}
+
+//export Join
+func Join(pdomain unsafe.Pointer, cb uint, eb uint) {
+	d := *(*core.Domain)(pdomain)
+
+	if c, err := goRiffle.Open(fabric); err != nil {
+		d.GetApp().CallbackSend(eb, err.Error())
+	} else {
+		if err := d.Join(c); err != nil {
+			d.GetApp().CallbackSend(eb, err.Error())
+		} else {
+			d.GetApp().CallbackSend(cb)
+		}
+	}
+}
+
+//export Subscribe
+func Subscribe(pdomain unsafe.Pointer, cb uint, endpoint *C.char) {
+	d := *(*core.Domain)(pdomain)
+	go func() {
+		d.Subscribe(C.GoString(endpoint), cb, make([]interface{}, 0))
+	}()
+}
+
+//export Register
+func Register(pdomain unsafe.Pointer, cb uint, endpoint *C.char) {
+	d := *(*core.Domain)(pdomain)
+	go func() {
+		d.Register(C.GoString(endpoint), cb, make([]interface{}, 0))
+	}()
+}
+
+// Bytes for args!
+//export Publish
+func Publish(pdomain unsafe.Pointer, cb uint, endpoint *C.char, args []byte) {
+	d := *(*core.Domain)(pdomain)
+	go func() {
+		d.Publish(C.GoString(endpoint), cb, core.MantleUnmarshal(string(args)))
+	}()
+}
+
+//export Call
+func Call(pdomain unsafe.Pointer, cb uint, endpoint *C.char, args []byte) {
+	d := *(*core.Domain)(pdomain)
+	go func() {
+		d.Call(C.GoString(endpoint), cb, core.MantleUnmarshal(string(args)))
+	}()
+}
+
+//export Yield
+func Yield(pdomain unsafe.Pointer, request uint, args []byte) {
+	d := *(*core.Domain)(pdomain)
+	go func() {
+		d.GetApp().Yield(request, core.MantleUnmarshal(string(args)))
+	}()
+}
+
+//export Unsubscribe
+func Unsubscribe(pdomain unsafe.Pointer, endpoint *C.char) {
+	d := *(*core.Domain)(pdomain)
+	go func() {
+		d.Unsubscribe(C.GoString(endpoint))
+	}()
+}
+
+//export Unregister
+func Unregister(pdomain unsafe.Pointer, endpoint *C.char) {
+	d := *(*core.Domain)(pdomain)
+	go func() {
+		d.Unregister(C.GoString(endpoint))
+	}()
+}
+
+//export Leave
+func Leave(pdomain unsafe.Pointer) {
+	d := *(*core.Domain)(pdomain)
+	go func() {
+		d.Leave()
+	}()
+}
 
 // //export Subscribe
 // func Subscribe(pdomain unsafe.Pointer, endpoint *C.char, data []bytes) []byte {
@@ -70,7 +142,7 @@ func Subdomain(pdomain unsafe.Pointer, name *C.char) unsafe.Pointer  {
 // func coreInvoke(operation func(string, uint, []interface{}) error, endpoint *C.char, args []interface{}) []byte {
 // 	cb, eb := core.NewID(), core.NewID()
 // 	go func() {
-// 		if err := operation(C.GoString(endpoint), cb, args); err != nil {
+// 		if err := operation(C.GoString(C.GoString(endpoint)), cb, args); err != nil {
 // 			man.InvokeError(eb, err.Error())
 // 		}
 // 	}()
@@ -179,38 +251,50 @@ func Subdomain(pdomain unsafe.Pointer, name *C.char) unsafe.Pointer  {
 // 	fmt.Println("Domain left!")
 // }
 
-
 //export SetLogLevelOff
-func SetLogLevelOff()   { core.LogLevel = core.LogLevelOff }
+func SetLogLevelOff() { core.LogLevel = core.LogLevelOff }
+
 //export SetLogLevelApp
-func SetLogLevelApp()   { core.LogLevel = core.LogLevelApp }
+func SetLogLevelApp() { core.LogLevel = core.LogLevelApp }
+
 //export SetLogLevelErr
-func SetLogLevelErr()   { core.LogLevel = core.LogLevelErr }
+func SetLogLevelErr() { core.LogLevel = core.LogLevelErr }
+
 //export SetLogLevelWarn
-func SetLogLevelWarn()  { core.LogLevel = core.LogLevelWarn }
+func SetLogLevelWarn() { core.LogLevel = core.LogLevelWarn }
+
 //export SetLogLevelInfo
-func SetLogLevelInfo()  { core.LogLevel = core.LogLevelInfo }
+func SetLogLevelInfo() { core.LogLevel = core.LogLevelInfo }
+
 //export SetLogLevelDebug
 func SetLogLevelDebug() { core.LogLevel = core.LogLevelDebug }
 
 //export SetFabricDev
-func SetFabricDev()        { fabric = core.FabricDev }
+func SetFabricDev() { fabric = core.FabricDev }
+
 //export SetFabricSandbox
-func SetFabricSandbox()    { fabric = core.FabricSandbox }
+func SetFabricSandbox() { fabric = core.FabricSandbox }
+
 //export SetFabricProduction
 func SetFabricProduction() { fabric = core.FabricProduction }
+
 //export SetFabricLocal
-func SetFabricLocal()      { fabric = core.FabricLocal }
+func SetFabricLocal() { fabric = core.FabricLocal }
+
 //export SetFabric
 func SetFabric(url string) { fabric = url }
 
 //export Application
 func Application(s string) { core.Application("%s", s) }
+
 //export Debug
-func Debug(s string)       { core.Debug("%s", s) }
+func Debug(s string) { core.Debug("%s", s) }
+
 //export Info
-func Info(s string)        { core.Info("%s", s) }
+func Info(s string) { core.Info("%s", s) }
+
 //export Warn
-func Warn(s string)        { core.Warn("%s", s) }
+func Warn(s string) { core.Warn("%s", s) }
+
 //export Error
-func Error(s string)       { core.Error("%s", s) }
+func Error(s string) { core.Error("%s", s) }
