708096adfa4ec38adfdee368bb63ae9b1210108e
damouse <damouse007@gmail.com>
2015-12-10 20:06:32 -0600

    gopy decided to crap out on me. Restabilized

--- core/pyMantle/main.go
+++ core/pyMantle/main.go
@@ -4,54 +4,105 @@ import (
 	"fmt"
 
 	"github.com/exis-io/core"
-	"github.com/exis-io/core/goRiffle"
 )
-
-type Domain interface {
-	Subscribe(string) (int, int)
-	// Register(string, uint, []interface{}) error
-
-	// Publish(string, uint, []interface{}) error
-	// Call(string, uint, []interface{}) ([]interface{}, error)
-
-	// Yield(uint, []interface{})
-
-	// Unsubscribe(string) error
-	// Unregister(string) error
-
-	// Join(Connection) error
-	// Leave() error
-}
-
-type App struct {
-	app  core.App
-	conn *goRiffle.WebsocketConnection
-	recv chan []byte
-}
-
-type domain struct {
-	core core.Domain
-}
-
-// var man = &Mantle{
-// 	recv: make(chan []byte),
-// }
 
 func main() {}
 
-func (m *App) NewDomain(name string) Domain {
-	fmt.Println("NewDomain called!")
+// type Domaine interface {
+// 	Subscribe(string) (int, int)
+// 	// Register(string, uint, []interface{}) error
 
-	if m.app == nil {
-		m.app = core.NewApp()
-	}
+// 	// Publish(string, uint, []interface{}) error
+// 	// Call(string, uint, []interface{}) ([]interface{}, error)
 
-	d := domain{
-		core: m.app.NewDomain(name, m),
-	}
+// 	// Yield(uint, []interface{})
 
-	return d
+// 	// Unsubscribe(string) error
+// 	// Unregister(string) error
+
+// 	Join() (uint, uint)
+// 	// Leave() error
+// }
+
+// type AppIface interface {
+// 	NewDomain(string) Domain
+// }
+
+// type App struct {
+// 	coreApp core.App
+// 	conn    *goRiffle.WebsocketConnection
+// }
+
+// type Domain struct {
+// 	*App
+// 	core core.Domain
+// }
+
+// func (a *App) NewDomain(name string) Domain {
+// 	if a.coreApp == nil {
+// 		a.coreApp = core.NewApp()
+// 	}
+
+// 	d := Domain{
+// 		App:  a,
+// 		core: a.coreApp.NewDomain(name),
+// 	}
+
+// 	return d
+// }
+
+// Exmaple
+// Iface has a single F() method
+type AppIface interface {
+	NewDomain()
 }
+
+type DomainIface interface {
+	Join()
+}
+
+// T implements Iface
+type App struct{}
+type Domain struct{}
+
+func (t *App) NewDomain() Domain {
+	fmt.Printf("App.NewDomain\n")
+	return Domain{}
+}
+
+func (t *Domain) Join() {
+	fmt.Printf("Domain.Join\n")
+}
+
+// func (d *Domain) Join() int {
+
+// 	if d.App.conn != nil {
+// 		// man.InvokeError(eb, "Connection is already open!")
+// 		core.Warn("Connection is already open!")
+// 	}
+
+// 	if c, err := goRiffle.Open(core.DevFabric); err != nil {
+// 		// man.InvokeError(eb, err.Error())
+// 		core.Warn("Unable to open connection: %s", err.Error())
+// 	} else {
+// 		d.App.conn = c
+// 		c.App = d.App.app
+
+// 		if err := d.core.Join(c); err != nil {
+// 			core.Warn("Unable to join! %s", err)
+// 			// man.InvokeError(eb, err.Error())
+// 		} else {
+// 			core.Info("Joined!")
+// 			// man.Invoke(cb, nil)
+// 		}
+// 	}
+
+// 	return 0
+// }
+
+// func (d Domain) Subscribe(endpoint string) (int, int) {
+// 	return 0, 0
+// }
 
 // Applys a set of parameters to the core domain using the passed function
 // func domainCall(operation func(), endpoint string, args []interface{}) (uint, uint) {
@@ -64,10 +115,6 @@ func (m *App) NewDomain(name string) Domain {
 
 // 	return marshall([]uint{cb, eb})
 // }
-
-func (d domain) Subscribe(endpoint string) (int, int) {
-	return 0, 0
-}
 
 /*
 //export Register
@@ -115,34 +162,6 @@ func Unregister(pdomain unsafe.Pointer, e string) {
 	d.Unregister(e))
 }
 
-//export Join
-func Join(pdomain unsafe.Pointer) []byte {
-	d := *(*core.Domain)(pdomain)
-	cb, eb := core.NewID(), core.NewID()
-
-	go func() {
-		if man.conn != nil {
-			man.InvokeError(eb, "Connection is already open!")
-		}
-
-		if c, err := goRiffle.Open(core.DevFabric); err != nil {
-			man.InvokeError(eb, err.Error())
-		} else {
-			man.conn = c
-			c.App = man.app
-
-			if err := d.Join(c); err != nil {
-				core.Warn("Unable to join! %s", err)
-				man.InvokeError(eb, err.Error())
-			} else {
-				core.Info("Joined!")
-				man.Invoke(cb, nil)
-			}
-		}
-	}()
-
-	return marshall([]uint{cb, eb})
-}
 
 //export Leave
 func Leave(pdomain unsafe.Pointer) {
@@ -183,7 +202,6 @@ func unmarshall() {
 // m.recv <- marshall([]interface{}{id, s})
 // }
 
-//export SetLoggingLevel
 func SetLoggingLevel(l int) {
 	core.LogLevel = l
 }
