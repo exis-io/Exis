e471f5e81e177f19afbd54712d3ea933c8d21e70
damouse <mickey.barboi@gmail.com>
2015-12-14 14:13:59 -0600

    updated with even more kittens

--- swift/swiftRiffle/riffle.swift
+++ swift/swiftRiffle/riffle.swift
@@ -1027,58 +1027,6 @@ func decode(p: GoSlice) -> (Int64, [Any]) {
 // Deferred.swift
 //////////////////////////////////////
 
-public class Deferred {
-    var _callback: ((AnyObject?) -> AnyObject?)? = nil
-    var _errback: ((AnyObject?) -> AnyObject?)? = nil
-    
-    var fired = false
-    
-    public init() {}
-    
-    public func callback(args: AnyObject? = nil) -> AnyObject? {
-        if fired {
-            print("Defereds can only fire once!")
-            return nil
-        }
-        
-        fired = true
-        
-        if let cb = _callback {
-            return cb(args)
-        } else {
-            print("No callback registered")
-            return nil
-        }
-    }
-    
-    public func errback(args: AnyObject? = nil) -> AnyObject? {
-        if fired {
-            print("Defereds can only fire once!")
-            return nil
-        }
-        
-        fired = true
-        
-        if let cb = _errback {
-            return cb(args)
-        } else {
-            print("No callback registered")
-            return nil
-        }
-    }
-    
-    
-    public func addCallback(cb: (AnyObject?) -> (AnyObject?)) -> Deferred {
-        _callback = cb
-        return self
-    }
-    
-    public func addErrback(cb: (AnyObject?) -> (AnyObject?)) -> Deferred {
-        _errback = cb
-        return self
-    }
-}
-
 func invocation(slice: GoSlice) -> (Int64, Int64) {
     let (i, a) = decode(slice)
 
@@ -1098,6 +1046,8 @@ public class Domain {
     
     
     public init(name: String) {
+        let d = Deferred()
+        
         mantleDomain = NewDomain(name.cString())
         // delegate = self
     }
