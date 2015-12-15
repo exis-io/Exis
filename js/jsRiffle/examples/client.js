4d1c695dbe8079f70b871be935afed2ba04957a3
damouse <damouse007@gmail.com>
2015-12-15 04:38:29 -0600

    need to set up receive loop

--- js/jsRiffle/examples/client.js
+++ js/jsRiffle/examples/client.js
@@ -1,29 +1,16 @@
 var riffle = require('jsriffle');
-// riffle.setDevFabric();
 
-// var app = new riffle.Domain("xs.demo");
-// var me = app.subdomain("client");
-// var server = app.subdomain("server");
+riffle.SetFabricLocal();
+riffle.SetLogLevelDebug();
 
+var app = riffle.Domain("xs.damouse");
+var receiver = app.Subdomain("alpha");
+var me = app.Subdomain("beta");
 
-// me.onJoin = function() {
-//     console.log("Domain " + this.domain + " joined");
+me.onJoin = function() {
+    console.log("Sender Joined");
 
-//     server.publish('sub', 'Hello,', 'You scallywag!');
+    receiver.Publish("sub", 1, 2, 3)
+};
 
-//     server.call('register', "Ping, ", "she wrote").then(
-//       function (res) {
-//          console.log("Call returned: ", res);
-//       }
-//    );
-// };
-
-
-// me.join();
-
-
-var domain = riffle.Domain("xs.damouse.js.beta")
-
-domain.Join()
-
-domain.Publish("xs.damouse.js.alpha/sub")
+me.Join()
