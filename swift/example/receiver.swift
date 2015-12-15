66a1e81996f77062fec1686592fd0af05567f938
damouse <damouse007@gmail.com>
2015-12-14 22:40:41 -0600

    sending and receiving on 2.2

---
+++ swift/example/receiver.swift
@@ -0,0 +1,32 @@
+
+import riffle
+
+riffle.SetLogLevelDebug()
+riffle.SetFabricLocal()
+
+
+class Receiver: riffle.Domain, riffle.Delegate {
+
+    override func onJoin() {
+        print("Receiver joined!")
+        
+        register("reg") { (args: Any) -> Any? in
+            print("Received call! Args: \(args)")
+            return nil
+        }
+        
+
+        subscribe("sub") { (args: Any) in
+            print("Received publish! \(args)")
+        }
+    }
+    
+    override func onLeave() {
+        print("Receiver left!")
+    }
+}
+
+
+let receiver = Receiver(name: "xs.damouse.alpha")
+receiver.delegate = receiver
+receiver.join()
