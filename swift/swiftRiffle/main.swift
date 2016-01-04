4f4ca99904470327fcf732a92f0e2c211fd4519e
Mickey Barboi <damouse007@gmail.com>
2016-01-04 15:03:49 -0600

    final changes

--- swift/swiftRiffle/main.swift
+++ swift/swiftRiffle/main.swift
@@ -2,54 +2,27 @@
 
 import Foundation
 
+SetLogLevelDebug()
+SetFabricLocal()
+
 
 class Dog: Model {
     var name = "Fido"
     var age = 43
 }
 
-/*
-var holder: (([Any]) -> ())? = nil
-
-func newcumin<A: Property, B: Property, C: Property>(fn: (A, B, C) -> ()) {
-    // Assume we're getting primitive types well-constructed
-    // Detect collections and objects, build them appropriately
-    
-    // NOTE: Collections only need to be detected for nested objects
-    
-    // Construct cumin strings here and pass to core-- repr might be betterer
-    //print("C is a model: \(C.isModel())")
-    
-    holder = { args in
-        // Coerce types, constructing them if needed, and call the function with the results
-        fn(A.create(args[0]) as! A, B.create(args[1]) as! B, C.create(args[2]) as! C)
-    }
-}
-
-func add(a: Int, b: [String], c: Dog) {
-    print("Adding: \(a), with list: \(b), and pup: \(c.description)")
-}
-
-newcumin(add)
-
-holder!([1, ["Hey", "There"], ["name": "Billiam", "age": 88]])
-*/
-
-
-
-SetLogLevelDebug()
-SetFabricLocal()
-
-
 class Sender: Domain {
     
     override func onJoin() {
+        // Create an object
         let dog = Dog()
         dog.name = "Billiam"
         dog.age = 88
         
+        // Publish the object and assorted other arguments
         publish("xs.damouse.alpha/sub", 1, ["Hey", "There"], dog)
         
+        // Ca; with assorted arguments, stipulating the resulting return types
         call("xs.damouse.alpha/reg", "Johnathan", "Seed") { (a: String) in
             print("Call received: ", a)
         }
@@ -80,11 +53,9 @@ class Receiver: Domain {
     }
 }
 
-//Receiver(name: "xs.damouse.alpha").join()
-
+// Allows this script to act as a receiver or a sender
 let startSender = NSProcessInfo.processInfo().environment["SENDER"] != nil
 
-// Start the scripts
 if startSender {
     print("Starting Sender")
     Sender(name: "xs.damouse.beta").join()
