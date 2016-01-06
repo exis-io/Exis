ece94f5d5036290bf740b6d0764a70f71fcee06f
damouse <damouse007@gmail.com>
2016-01-05 18:28:32 -0600

    cleanup on arbiter tests

--- swift/example/receiver.swift
+++ swift/example/receiver.swift
@@ -19,6 +19,7 @@ class Receiver: Riffle.Domain, Riffle.Delegate {
             print("An error occured", reason)
         }
         
+        // Example Pub/Sub Basic - This is a basic version of a pub/sub
         subscribe("sub") { (a: Int, b: [String], c: Dog) in
             print("Received publish: \(a), with list: \(b), and pup: \(c.description)")
         }.then {
@@ -26,18 +27,7 @@ class Receiver: Riffle.Domain, Riffle.Delegate {
         }.error { reason in
             print("An error occured", reason)
         }
-
-
-        // register("reg") { (args: Any) -> Any? in
-        //     print("Received call! Args: \(args)")
-        //     return nil
-        // }
-        
-        // // Example Pub/Sub Basic - This is a basic version of a pub/sub
-        // subscribe("sub") { (args: Any) in       // Expects an Any, like "Hello"
-        //     print("Received publish! \(args)")
-        // }
-        // // End Example Pub/Sub Basic
+        // End Example Pub/Sub Basic
     }
     
     override func onLeave() {
--- swift/example/sender.swift
+++ swift/example/sender.swift
@@ -5,30 +5,21 @@ class Sender: Riffle.Domain, Riffle.Delegate  {
     override func onJoin() {
         print("Sender joined")
 
-        // Create an object
+        // Example Pub/Sub Basic - This is a basic version of a pub/sub
         let dog = Dog()
         dog.name = "Billiam"
         dog.age = 88
-        
-        // Publish the object and assorted other arguments
+
         publish("xs.damouse.alpha/sub", 1, ["Hey", "There"], dog).then {
             print("Publish succeeded")
         }.error { reason in
             print("An error occured", reason)
         }
+        // End Example Pub/Sub Basic
         
-        // Call with assorted arguments, stipulating the resulting return types
         call("xs.damouse.alpha/reg", "Johnathan", "Seed").then { (a: String) in
             print("Call received: ", a)
         }
-
-        // // Example Pub/Sub Basic - This is a basic version of a pub/sub
-        // publish("xs.damouse.alpha/sub", 1, "2", true)
-        // // End Example Pub/Sub Basic
-        
-        // call("xs.damouse.alpha/reg", "Johnathan", "Seed") { returnArgs in
-        //     print("Call received result \(returnArgs)")
-        // }
     }
     
     override func onLeave() {
