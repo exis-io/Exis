94886247994d8e0ba124e2cf10c0e5fd3daea91c
damouse <damouse007@gmail.com>
2016-01-07 16:29:26 -0600

    name change between versions

--- swift/example/sender.swift
+++ swift/example/sender.swift
@@ -7,11 +7,11 @@ class Sender: Riffle.Domain, Riffle.Delegate  {
         print("Sender joined")
 
         // Example Pub/Sub Basic - This is a basic version of a pub/sub
-        publish("xs.demo.test.example/basicSub", "Hello")
+        publish("xs.test.example/basicSub", "Hello")
         // End Example Pub/Sub Basic
         
         // Example Reg/Call Basic 1 - This is a basic reg/call
-        call("xs.demo.test.example/basicReg", "Hello").then { (a: String) in
+        call("xs.test.example/basicReg", "Hello").then { (a: String) in
             print("\(a)") // Expects a String, like "Hello World"
         }
         // End Example Reg/Call Basic 1
