7cc87f833daaa4b325e6c5c7a0d145753b41cf0f
Dale Willis <dalefwillis@gmail.com>
2016-01-11 19:43:51 -0600

    Adding some tour basics exis-code tags and the repl-template

---
+++ swift/example/repl-template.swift
@@ -0,0 +1,22 @@
+// Example REPL Template - The REPL code
+// ARBITER set action template
+import Foundation
+import Riffle
+
+// This connects us to the sandbox fabric
+Riffle.SetFabricSandbox()
+
+class ExisBackend: Riffle.Domain, Riffle.Delegate  {
+    
+    // When the connection is established this function is called
+    override func onJoin() {
+        
+        // Exis code goes here
+    
+    }
+}
+
+// Setup the domain here
+ExisBackend(name: "xs.demo.test.backend").join()
+
+// End Example REPL Template
