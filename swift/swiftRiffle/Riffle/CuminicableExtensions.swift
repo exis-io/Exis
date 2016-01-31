8220cd68c49e3bc2aa4b5552782be6fa92aa33ac
damouse <damouse@damouses-Mac.local>
2016-01-31 12:29:25 -0600

    array serialization fix

--- swift/swiftRiffle/Riffle/CuminicableExtensions.swift
+++ swift/swiftRiffle/Riffle/CuminicableExtensions.swift
@@ -217,14 +217,24 @@ extension Array : Property, BaseConvertible {
     
     public static func deserialize(from: Any) -> Any {
         if let arr = from as? [Any] {
-            return arr.map { $0 as! Element }
+            var ret: [Element] = []
+            
+            // Reconstruct values within the array
+            for element in arr {
+                if let child = Generator.Element.self as? Convertible.Type {
+                    ret.append(child.deserialize(element) as! Element)
+                }
+            }
+
+            return ret
         }
         
+        // TOOD: This is a silent error case!
         return from
     }
     
     public func serialize() -> Any {
-        // Apply recursive serialization here
+        // TODO: Apply recursive serialization here
         return self
     }
     
--- swift/swiftRiffle/main.swift
+++ swift/swiftRiffle/main.swift
@@ -7,6 +7,12 @@ SetLogLevelDebug()
 //SetLogLevelInfo()
 SetFabricLocal()
 
+/*
+Allow any
+Allow call returns
+Properly formatted errors
+
+*/
 class Dog: Model {
     var name = "Fido"
     var age = 43
@@ -31,21 +37,21 @@ class Receiver: Domain {
         // Pub Sub Success Cases
             
         // No arguments
-        subscribe("subscribeNothing") {
-            print("SUCCESS --- 1-1")
-        }
-        
-        // Primitive Types
-        subscribe("subscribePrimitives") { (a: Int, b: Float, c: Double, d: String, e: Bool) in
-            print("SUCCESS --- 1-2")
-            //print("1 : Sub receiving single types:", a, b, c, d, e)
-            
-            assert(a == 1)
-            assert(b == 2.2)
-            assert(c == 3.3)
-            assert(d == "4")
-            assert(e == true)
-        }
+//        subscribe("subscribeNothing") {
+//            print("SUCCESS --- 1-1")
+//        }
+//        
+//        // Primitive Types
+//        subscribe("subscribePrimitives") { (a: Int, b: Float, c: Double, d: String, e: Bool) in
+//            print("SUCCESS --- 1-2")
+//            //print("1 : Sub receiving single types:", a, b, c, d, e)
+//            
+//            assert(a == 1)
+//            assert(b == 2.2)
+//            assert(c == 3.3)
+//            assert(d == "4")
+//            assert(e == true)
+//        }
         
         // Arrys of simple types
         subscribe("subscribeArrays") { (a: [Int], b: [Float], c: [Double], d: [String], e: [Bool]) in
@@ -69,40 +75,40 @@ class Receiver: Domain {
         
         // Reg/Call Success Cases
         // No arguments
-        register("registerNothing") {
-            print("SUCCESS --- 2-1")
-            return
-        }
-        
-        
-        // Simple Types
-        // FAIL when returning the types back to the client 
-        // FAIL with no cumin enforcement present
-        register("registerPrimitives") { (a: Int, b: Float, c: Double, d: String, e: Bool)  in
-            print("SUCCESS --- 2-2")
-            //print("Received: \(a) \(b) \(c) \(d) \(e), expecting 1 2.2 3.3 4 true")
-            
-            assert(a == 1)
-            assert(b == 2.2)
-            assert(c == 3.3)
-            assert(d == "4")
-            assert(e == true)
-            
-//            return [a, b, c, d, e]
-        }
-        
-        // Collections of simple types
-        register("registerArrays") { (a: [Int], b: [Float], c: [Double], d: [String], e: [Bool]) in
-            print("SUCCESS --- 2-3")
-            //print("Received: \(a) \(b) \(c) \(d) \(e), expecting 1 2.2 3.3 4 true")
-            
-            assert(a == [1, 2])
-            assert(b == [2.2, 3.3])
-            assert(c == [4.4, 5.5])
-            assert(d == ["6", "7"])
-            assert(e == [true, false])
-        }
-        
+//        register("registerNothing") {
+//            print("SUCCESS --- 2-1")
+//            return
+//        }
+//        
+//        
+//        // Simple Types
+//        // FAIL when returning the types back to the client 
+//        // FAIL with no cumin enforcement present
+//        register("registerPrimitives") { (a: Int, b: Float, c: Double, d: String, e: Bool)  in
+//            print("SUCCESS --- 2-2")
+//            //print("Received: \(a) \(b) \(c) \(d) \(e), expecting 1 2.2 3.3 4 true")
+//            
+//            assert(a == 1)
+//            assert(b == 2.2)
+//            assert(c == 3.3)
+//            assert(d == "4")
+//            assert(e == true)
+//            
+////            return [a, b, c, d, e]
+//        }
+//        
+//        // Collections of simple types
+//        register("registerArrays") { (a: [Int], b: [Float], c: [Double], d: [String], e: [Bool]) in
+//            print("SUCCESS --- 2-3")
+//            //print("Received: \(a) \(b) \(c) \(d) \(e), expecting 1 2.2 3.3 4 true")
+//            
+//            assert(a == [1, 2])
+//            assert(b == [2.2, 3.3])
+//            assert(c == [4.4, 5.5])
+//            assert(d == ["6", "7"])
+//            assert(e == [true, false])
+//        }
+//        
         
         // Riffle Model objects with returns
 //            register("registerModel") { (d: Dog) -> Dog in
@@ -180,44 +186,43 @@ class Sender: Domain {
         // Pub Sub Success Cases
         // No args
 //        print("Receiver: \(receiver)")
-        receiver.publish("subscribeNothing")
-        
-//        // Primitive Types
-        receiver.publish("subscribePrimitives", 1, 2.2, 3.3, "4", true)
-//
+//        receiver.publish("subscribeNothing")
+//        
+////        // Primitive Types
+//        receiver.publish("subscribePrimitives", 1, 2.2, 3.3, "4", true)
+////
 //        // Arrys of simple types
-        receiver.publish("subscribeArays", [1, 2], [2.2, 3.3], [4.4, 5.5], ["6", "7"], [true, false])
+        receiver.publish("subscribeArrays", [1, 2], [2.2, 3.3], [4.4, 5.5], ["6", "7"], [true, false])
 //
 //        
 //        // Reg/Call Success Cases
 //        // No arguments
-        receiver.call("registerNothing").then {
-            assert(true)
-        }
-//
-//        // Primitive Types
-        receiver.call("registerPrimitives", 1, 2.2, 3.3, "4", true)
-//            .then { (a: Int, b: Float, c: Double, d: String, e: Bool) in
-//            assert(a == 1)
-//            assert(b == 2.2)
-//            assert(c == 3.3)
-//            assert(d == "4")
-//            assert(e == true)
+//        receiver.call("registerNothing").then {
+//            assert(true)
 //        }
-
-        // Collections of simple types
-        receiver.call("registerPrimitives", [1, 2], [2.2, 3.3], [4.4, 5.5], ["6", "7"], [true, false]).then { (a: [Int], b: [Float], c: [Double], d: [String], e: [Bool]) in
-            assert(a == [1, 2])
-            assert(b == [2.2, 3.3])
-            assert(c == [4.4, 5.5])
-            assert(d == ["6", "7"])
-            assert(e == [true, false])
-        }.error { reason in
-            // TODO: the reason itself is not given, instead its the class of argument
-            print("FAILURE ON CALL --- 2-2")
-            print("\tREASON: \(reason)")
-        }
-
+////
+////        // Primitive Types
+//        receiver.call("registerPrimitives", 1, 2.2, 3.3, "4", true)
+////            .then { (a: Int, b: Float, c: Double, d: String, e: Bool) in
+////            assert(a == 1)
+////            assert(b == 2.2)
+////            assert(c == 3.3)
+////            assert(d == "4")
+////            assert(e == true)
+////        }
+//
+//        // Collections of simple types
+//        receiver.call("registerPrimitives", [1, 2], [2.2, 3.3], [4.4, 5.5], ["6", "7"], [true, false]).then { (a: [Int], b: [Float], c: [Double], d: [String], e: [Bool]) in
+//            assert(a == [1, 2])
+//            assert(b == [2.2, 3.3])
+//            assert(c == [4.4, 5.5])
+//            assert(d == ["6", "7"])
+//            assert(e == [true, false])
+//        }.error { reason in
+//            // TODO: the reason itself is not given, instead its the class of argument
+//            print("FAILURE ON CALL --- 2-2")
+//            print("\tREASON: \(reason)")
+//        }
     }
     
     override func onLeave() {
