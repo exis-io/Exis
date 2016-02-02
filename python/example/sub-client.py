748ed676ab7b2ef2a04d930e9a16b5994d8acdc0
Lance Hartung <lance@exis.io>
2016-02-02 10:28:44 -0600

    Fixed Pub/Sub Basic example.

--- python/example/sub-client.py
+++ python/example/sub-client.py
@@ -10,15 +10,15 @@ class Send(riffle.Domain):
     def onJoin(self):
 
         # Example Pub/Sub Basic - This is a basic pub/sub
-        self.publish("basicSub", "Hello")
+        backend.publish("basicSub", "Hello")
         # End Example Pub/Sub Basic
 
         # Example Pub/Sub Objects - This tests sending an object using pub/sub
         class Stuff(riffle.ModelObject):
             name = "This guy"
-            
+
         s = Stuff()
-        self.publish("objectSub", s)
+        backend.publish("objectSub", s)
         # End Example Pub/Sub Objects
 
         # self.leave()
