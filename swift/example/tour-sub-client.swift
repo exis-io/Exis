bd18b3bc22693e7a23006b2346e719c212f0b93a
damouse <damouse007@gmail.com>
2016-03-10 15:33:34 -0600

    removed manual arbiter tests from examples folder

--- swift/example/tour-sub-client.swift
+++ swift/example/tour-sub-client.swift
@@ -3,19 +3,21 @@ import Riffle
 class TourSubClient: Domain, Delegate {
 
     override func onJoin() {
-        /////////////////////////////////////////////////////////////////////////////////////
+        
         // Example Tour Pub/Sub Lesson 1 - our first basic example
         backend.publish("myFirstSub", "Hello")
         // End Example Tour Pub/Sub Lesson 1
 
-        /////////////////////////////////////////////////////////////////////////////////////
+
         // Example Tour Pub/Sub Lesson 2 Works - type enforcement good
         backend.publish("iWantStrings", "Hi")
         // End Example Tour Pub/Sub Lesson 2 Works
 
+
         // Example Tour Pub/Sub Lesson 2 Fails - type enforcement bad
         backend.publish("iWantInts", "Hi")
         // End Example Tour Pub/Sub Lesson 2 Fails
+
 
         print("___SETUPCOMPLETE___")
     }
