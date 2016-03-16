6140c4e94aa92ac61db79caf87c71d8998e41270
damouse <damouse@damouses-Mac.local>
2016-03-16 18:51:07 -0500

    soft cumin missing a case for doubles

--- core/cumin.go
+++ core/cumin.go
@@ -154,7 +154,7 @@ func primitiveCheck(expected string, argument reflect.Kind) error {
 	if argument == reflect.Bool && expected == "bool" ||
 		argument == reflect.String && expected == "str" ||
 		argument == reflect.Float64 && (expected == "float" || expected == "int" || expected == "double") ||
-		argument == reflect.Int && (expected == "float" || expected == "int") ||
+		argument == reflect.Int && (expected == "float" || expected == "int" || expected == "double") ||
 		argument == reflect.Map && expected == "dict" {
 		return nil
 	}
