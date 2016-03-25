4a08ea3a53b6330c61e09136e85daa8cc06c5c80
damouse <damouse007@gmail.com>
2016-03-25 14:33:31 -0500

    nested deferred return test

--- js/example/test-backend.js
+++ js/example/test-backend.js
@@ -1,4 +1,3 @@
-
 var riffle = require('jsriffle');
 
 riffle.setFabricLocal();
@@ -9,23 +8,31 @@ var backend = app.subdomain("backend");
 var client = app.subdomain("client");
 
 backend.onJoin = function() {
-    // Example Test Client leave - does client leave properly
-    this.register("testClientLeave", riffle.want(function(s) {
-        console.log(s); // Expects a String, like "Leave"
-        return "Leaving";
-    }, String));
-    // End Example Test Client leave
-    
-    // Example Test Backend leave - does client leave properly
-    var self = this;
-    this.onLeave = function() {
-        console.log("On Leave Triggered"); // Expects a String, like "On Leave Triggered"
-    };
-    this.register("testBackendLeave", riffle.want(function(s) {
-        self.leave();
-        return "Leaving";
-    }, String));
-    // End Example Test Backend leave
+  // Example Test Client leave - does client leave properly
+  this.register("testClientLeave", riffle.want(function(s) {
+    console.log(s); // Expects a String, like "Leave"
+    return "Leaving";
+  }, String));
+  // End Example Test Client leave
+
+  // Example Test Backend leave - does client leave properly
+  var self = this;
+  this.onLeave = function() {
+    console.log("On Leave Triggered"); // Expects a String, like "On Leave Triggered"
+  };
+  this.register("testBackendLeave", riffle.want(function(s) {
+    self.leave();
+    return "Leaving";
+  }, String));
+  // End Example Test Backend leave
+
+  // Example Test Returning Nested Deferred - demonstrates returning nested deferreds
+  this.register("testBackendNested", riffle.want(function(s) {
+    return sender.call("testClientNested", s + ", second").then(function(a) {
+      return a + ", fourth"
+    });
+  }, String));
+  // End Example Test Returning Nested Deferred
 
 };
 
--- js/example/test-client.js
+++ js/example/test-client.js
@@ -8,29 +8,42 @@ var backend = app.subdomain("backend");
 var client = app.subdomain("client");
 
 client.onJoin = function() {
-    /////////////////////////////////////////////////////////////////////////////////////
-    // Example Test Client leave - does the client side leave properly
-    var self = this;
-    this.onLeave = function() {
-        console.log("On Leave Triggered"); // Expects a String, like "On Leave Triggered"
-    };
-    backend.call("testClientLeave", "Leave").want(String).then(function (s) {
-        self.leave();
+  /////////////////////////////////////////////////////////////////////////////////////
+  // Example Test Client leave - does the client side leave properly
+  var self = this;
+  this.onLeave = function() {
+    console.log("On Leave Triggered"); // Expects a String, like "On Leave Triggered"
+  };
+  backend.call("testClientLeave", "Leave").want(String).then(function(s) {
+      self.leave();
     },
-    function (err) {
-        console.log("ERROR: ", err);
+    function(err) {
+      console.log("ERROR: ", err);
     });
-    // End Example Test Client leave
-    
-    // Example Test Backend leave - does the client side leave properly
-    backend.call("testBackendLeave", "Leave").want(String).then(function (s) {
-        console.log("GOT: ", s);
-    },
-    function (err) {
-        console.log("ERROR: ", err);
-    });
-    // End Example Test Backend leave
+  // End Example Test Client leave
 
+  // Example Test Backend leave - does the client side leave properly
+  backend.call("testBackendLeave", "Leave").want(String).then(function(s) {
+      console.log("GOT: ", s);
+    },
+    function(err) {
+      console.log("ERROR: ", err);
+    });
+  // End Example Test Backend leave
+
+  // Example Test Returning Nested Deferred - demonstrates returning nested deferreds
+  this.register("testClientNested", riffle.want(function(s) {
+    return s + ", third"
+  }, String));
+
+  receiver.call("testBackendNested", "first").then(function(a) {
+      console.log(a); // Expects a String, like "first, second, third, fourth"
+    },
+    function(err) {
+      console.log("ERROR: ", err);
+    });
+
+  // End Example Test Returning Nested Deferred
 };
 
 client.join()
