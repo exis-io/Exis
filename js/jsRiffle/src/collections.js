e9b32510a57086d508ed6819c270163b5829de70
Nicholas Hyatt <nhyatt@wisc.edu>
2016-03-26 23:09:12 -0500

    fixed collections.js to take variadic positional args

--- js/jsRiffle/src/collections.js
+++ js/jsRiffle/src/collections.js
@@ -23,87 +23,85 @@ function Collection(storage, name){
   this.storage = storage;
 }
 
-Collection.prototype.create_index = function(keys){
-  return this.storage.call('collection/create_index', this.name, keys);
+function makeCall(func, args){
+  var args = Array.prototype.slice.call(args);
+  args.unshift(this.name);
+  args.unshift(func);
+  return this.storage.call.apply(this.storage, args);
+}
+
+Collection.prototype.create_index = function(){
+  return makeCall.bind(this, 'collection/create_index', arguments)();
 };
 
-Collection.prototype.delete_many = function(filter){
-  return this.storage.call('collection/delete_many', this.name, filter);
+Collection.prototype.delete_many = function(){
+  return makeCall.bind(this, 'collection/delete_many', arguments)();
 };
 
-Collection.prototype.delete_one = function(filter){
-  return this.storage.call('collection/delete_one', this.name, filter);
+Collection.prototype.delete_one = function(){
+  return makeCall.bind(this, 'collection/delete_one', arguments)();
 };
 
-Collection.prototype.distinct = function(key, filter){
-  return this.storage.call('collection/distinct', this.name, key, filter);
+Collection.prototype.distinct = function(){
+  return makeCall.bind(this, 'collection/distinct', arguments)();
 };
 
 Collection.prototype.drop = function(){
-  return this.storage.call('collection/drop', this.name);
+  return makeCall.bind(this, 'collection/drop', arguments)();
 };
 
-Collection.prototype.drop_index = function(index){
-  return this.storage.call('collection/drop_index', this.name, index);
+Collection.prototype.drop_index = function(){
+  return makeCall.bind(this, 'collection/drop_index', arguments)();
 };
 
 Collection.prototype.drop_indexes = function(){
-  return this.storage.call('collection/drop_indexes', this.name);
+  return makeCall.bind(this, 'collection/drop_indexes', arguments)();
 };
 
-Collection.prototype.find = function(filter, projection){
-  return this.storage.call('collection/find', this.name, filter, projection);
+Collection.prototype.find = function(){
+  return makeCall.bind(this, 'collection/find', arguments)();
 };
 
-Collection.prototype.find_one = function(filter, projection){
-  return this.storage.call('collection/find_one', this.name, filter, projection);
+Collection.prototype.find_one = function(){
+  return makeCall.bind(this, 'collection/find_one', arguments)();
 };
 
-Collection.prototype.find_one_and_delete = function(query, projection, sort){
-  return this.storage.call('collection/find_one_and_delete', this.name, query, projection, sort);
+Collection.prototype.find_one_and_delete = function(){
+  return makeCall.bind(this, 'collection/find_one_and_delete', arguments)();
 };
 
-Collection.prototype.find_one_and_replace = function(query, replacement, projection, sort){
-  return this.storage.call('collection/find_one_and_replace', this.name, query, replacement, projection, sort);
+Collection.prototype.find_one_and_replace = function(){
+  return makeCall.bind(this, 'collection/find_one_and_replace', arguments)();
 };
 
-Collection.prototype.find_one_and_update = function(query, update, projection, sort){
-  return this.storage.call('collection/find_one_and_replace', this.name, query, update, projection, sort);
+Collection.prototype.find_one_and_update = function(){
+  return makeCall.bind(this, 'collection/find_one_and_update', arguments)();
 };
 
-Collection.prototype.insert_one = function(doc){
-  return this.storage.call('collection/insert_one', this.name, doc);
+Collection.prototype.insert_one = function(){
+  return makeCall.bind(this, 'collection/insert_one', arguments)();
 };
 
-Collection.prototype.insert_many = function(documents, ordered){
-  return this.storage.call('collection/insert_many', this.name, documents, ordered);
+Collection.prototype.insert_many = function(){
+  return makeCall.bind(this, 'collection/insert_many', arguments)();
 };
 
 Collection.prototype.list_indexes = function(){
-  return this.storage.call('collection/list_indexes', this.name);
+  return makeCall.bind(this, 'collection/list_indexes', arguments)();
 };
 
-Collection.prototype.rename = function(target){
-  return this.storage.call('collection/rename', this.name, target);
+Collection.prototype.rename = function(){
+  return makeCall.bind(this, 'collection/rename', arguments)();
 };
 
 Collection.prototype.replace_one = function(){
-  var args = Array.prototype.slice.call(arguments);
-  args.unshift(this.name);
-  args.unshift('collection/replace_one');
-  return this.storage.call.apply(this.storage, args);
+  return makeCall.bind(this, 'collection/replace_one', arguments)();
 };
 
 Collection.prototype.update_one = function(){
-  var args = Array.prototype.slice.call(arguments);
-  args.unshift(this.name);
-  args.unshift('collection/update_one');
-  return this.storage.call.apply(this.storage, args);
+  return makeCall.bind(this, 'collection/update_one', arguments)();
 };
 
 Collection.prototype.update_many = function(){
-  var args = Array.prototype.slice.call(arguments);
-  args.unshift(this.name);
-  args.unshift('collection/update_many');
-  return this.storage.call.apply(this.storage, args);
+  return makeCall.bind(this, 'collection/update_many', arguments)();
 };
