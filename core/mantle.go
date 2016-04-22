d13d4cc27ece0d823db838d3f8b06caf678d1f53
damouse <damouse007@gmail.com>
2016-04-22 13:30:31 -0500

    mantle v2

---
+++ core/mantle.go
@@ -0,0 +1,201 @@
+package core
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// Mentle version 2. Reflects the core, handles invocations, and manages memory based on the reflected
+// api found in pkgreflect.go
+
+// JSON rpc invocation structure. The json must be a list with at least 4 elements
+// of the types below. All elements after the first 4 are parsed as arguments for the invocation
+// The first three fields always behave the same, the last two have semantic meaning based
+// on the Kind of the target
+//
+// Variable:
+//      Address is unused. If exactly one argument is passed the mantle will try to assign it
+//      to the variable. The current value of the variable is always returned.
+//
+//      Handle(`["CuminLevel", 0, 1, 0, 2]`) // Changes the CuminLevel
+//
+// Function:
+//      Address is unused for normal functions. All arguments are passed into that function.
+//      Constructors are functions named New[TypeName] and must return a pointer to the constructed
+//      type. In this case the address field is used to refer to that instance in the future.
+//      Clients are expected to pick well-distrubted random values for address
+//
+//      mantle.Handle(`["NewID", 10, 11, 0]`) // Normal Function
+//      mantle.Handle(`["NewApp", 10, 11, 12345]`) // Constructor for an App at address 12345
+//
+// Method:
+//      Target refers to the name of the method, address is the same as previously generated.
+//      All arguments are passed in as given.
+//
+//      mantle.Handle(`["SetState", 10, 11, 12345, 1]`) // calls SetState on App created previously
+//
+// Control:
+//      The current session is always at address 0. It implements control methods.
+//
+//      mantle.Handle(`["Free", 10, 11, 0, 12345]`) // dealloc an instance
+
+type rpc struct {
+	target  string        // A type, function, variable, or constant
+	cb      uint64        // The callback id to deliver the result on
+	eb      uint64        // errback id to deliver failure on
+	address uint64        // multiple-use field: "Pointer" when dealing with types, handler id for domain operations
+	args    []interface{} // Arguments to pass to the target
+}
+
+type Session interface {
+	Free(uint64)
+	// Close()
+	Handle(string) // This shouldn't really be an exported method, defer until the server
+}
+
+type session struct {
+	memory map[uint64]interface{} // "heap" space for this session
+}
+
+// Free the given object from memory. Does not check for presence
+func (s *session) Free(id uint64) {
+	fmt.Println("Freeing memory: ", id)
+	delete(s.memory, id)
+}
+
+// Creates a new session. The session has itself as the first memory address
+func NewSession() *session {
+	s := &session{make(map[uint64]interface{})}
+	s.memory[0] = s
+	return s
+}
+
+func (sess *session) Handle(line string) {
+	n, err := deserialize(line)
+
+	if err != nil {
+		fmt.Printf("Ignoring message %b. Error: %s\n", line, err.Error())
+		return
+	}
+
+	var result interface{}
+	var resultingId = n.cb
+
+	// Note: Types are used only for the sake of method reading-- instantiate types through their
+	// public constructors!
+
+	if m, ok := Variables[n.target]; ok {
+		result = handleVariable(m, n.args)
+	} else if m, ok := Consts[n.target]; ok {
+		result = m.Interface()
+	} else if m, ok := Functions[n.target]; ok {
+		if ret, err := handleFunction(m, n.args); err != nil {
+			resultingId = n.eb
+			result = err.Error()
+		} else {
+			if handleConstructor(n.target, n.address, sess.memory, ret) {
+				result = n.address
+			} else {
+				result = ret
+			}
+
+			resultingId = n.cb
+		}
+	} else if m, ok := sess.memory[n.address]; ok {
+		v := reflect.ValueOf(m).MethodByName(n.target)
+
+		var err error
+		if result, err = handleFunction(v, n.args); err != nil {
+			fmt.Printf("Method not successful: %s\n", err.Error())
+			resultingId = n.eb
+			result = err.Error()
+		} else {
+			resultingId = n.cb
+		}
+	} else {
+		fmt.Printf("Unknown invocation: %v\n", n)
+		return
+	}
+
+	dispatch(resultingId, result)
+}
+
+// Assign the given value to a variable and return its value. If we are passed "nil" as a
+// new value this is just a read-- dont try and set the value. Obviously this means nil is
+// not allowed as a variable value.
+// TODO: handle bad type conversions
+func handleVariable(v reflect.Value, n []interface{}) interface{} {
+	if len(n) == 1 {
+		c := reflect.ValueOf(n[0]).Convert(v.Elem().Type())
+		v.Elem().Set(c)
+	}
+
+	return v.Elem()
+}
+
+func handleFunction(fn reflect.Value, args []interface{}) ([]interface{}, error) {
+	return Cumin(fn.Interface(), args)
+}
+
+// Checks to see if a function invocation instantiated an object by checking the string of the target.
+// By convention constructors must be named "New[TypeName]" and return pointers.
+// If found and memory has been allocated for the given pointer, return true
+func handleConstructor(target string, address uint64, memory map[uint64]interface{}, invocationResult []interface{}) bool {
+	if len(invocationResult) != 1 {
+		return false
+	}
+
+	if strings.Index(target, "New") != -1 {
+		split := strings.Split(target, "New")
+
+		if len(split) == 2 && split[0] == "" {
+			memory[address] = invocationResult[0]
+			return true
+		}
+	}
+
+	return false
+}
+
+// Dispatch a callback to the given session
+func dispatch(id uint64, arg interface{}) {
+	fmt.Printf("Dispatching id: %d value: %v\n", id, arg)
+}
+
+func deserialize(j string) (*rpc, error) {
+	var d []interface{}
+	if e := json.Unmarshal([]byte(j), &d); e != nil {
+		return nil, fmt.Errorf("Unable to unmarshall data: %s\n", e)
+	}
+
+	n := &rpc{}
+
+	if s, ok := d[0].(string); !ok {
+		return nil, fmt.Errorf("Couldn't parse message-- incorrect type at position 0. Got %v", d[0])
+	} else {
+		n.target = s
+	}
+
+	if s, ok := d[1].(float64); !ok {
+		return nil, fmt.Errorf("Couldn't parse message-- incorrect type at position 1. Got %v", d[1])
+	} else {
+		n.cb = uint64(s)
+	}
+
+	if s, ok := d[2].(float64); !ok {
+		return nil, fmt.Errorf("Couldn't parse message-- incorrect type at position 2. Got %v", d[2])
+	} else {
+		n.eb = uint64(s)
+	}
+
+	if s, ok := d[3].(float64); !ok {
+		return nil, fmt.Errorf("Couldn't parse message-- incorrect type at position 3. Got %v", d[2])
+	} else {
+		n.address = uint64(s)
+	}
+
+	n.args = d[4:]
+	return n, nil
+}
