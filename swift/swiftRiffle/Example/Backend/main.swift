d692507bee10844ece6ea4d261b10c2c1235abda
damouse <damouse@damouses-Mac.local>
2016-04-24 11:40:13 -0500

    moving to persistence

--- swift/swiftRiffle/Example/Backend/main.swift
+++ swift/swiftRiffle/Example/Backend/main.swift
@@ -14,23 +14,20 @@ initTypes(External(String.self, String.self), External(Int.self, Int.self), Exte
 Riffle.setLogLevelDebug()
 Riffle.setFabricDev()
 
-// This is faking two seperate connections by creating another top level domain
-// Not intended for regular use
+// This is faking two seperate connections by creating another AppDomain. This is not intended for user functionality
 
 //let app = AppDomain(name: "xs.tester")
 //app.login()
 
-let app2 = AppDomain(name: "xs.tester")
-let receiver2 = Domain(name: "receiver", superdomain: app2)
-let sender2 = Sender(name: "sender", superdomain: app2, peer: receiver2)
-
-let app = AppDomain(name: "xs.tester")
-let receiver = Receiver(name: "receiver", superdomain: app, done: {
-    app2.login()
-})
-
-app.login()
-
+//let app2 = AppDomain(name: "xs.tester")
+//let receiver2 = Domain(name: "receiver", superdomain: app2)
+//let sender2 = Sender(name: "sender", superdomain: app2, peer: receiver2)
+//
+//let app = AppDomain(name: "xs.tester")
+//let receiver = Receiver(name: "receiver", superdomain: app, done: {
+//    app2.login()
+//})
+//
+//app.login()
+//
 //app.listen()
-
-NSRunLoop.currentRunLoop().run()
